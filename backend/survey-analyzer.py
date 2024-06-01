0a26ad3c5a77f48860fedb3c8393099c00ded0d0
ibrahim.kabbash <ibrahim.kabbash@espace.com.eg>
2024-06-01 14:44:58 +0300

    Create Flask app

--- backend/survey-analyzer.py
+++ backend/survey-analyzer.py
@@ -2,6 +2,8 @@ from azure.core.credentials import AzureKeyCredential
 from azure.ai.formrecognizer import DocumentAnalysisClient
 from azure.ai.textanalytics import TextAnalyticsClient
 from dotenv import load_dotenv
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 import json, os
 
 # Set endpoint, key and model from .env
@@ -16,7 +18,7 @@ def extract_document_feedback():
     # Connect to the document intelligence service
     document_analysis_client = DocumentAnalysisClient(endpoint=FORM_RECOGNIZER_ENDPOINT, credential=AzureKeyCredential(FORM_RECOGNIZER_KEY))
     # Path to your local document
-    document_path = "surveys/response-1.pdf"
+    document_path = "surveys/sample.pdf"
     # Open and read the document
     with open(document_path, "rb") as document:
         # Document analysis
@@ -31,20 +33,34 @@ def extract_document_feedback():
                 "Value": field_value,
                 "Confidence": field.confidence
             }
-        # Convert the output dictionary to JSON format then print for logging
+        # Convert the output dictionary to JSON format then print for logging purpose
         print("Formatted JSON output:\n" + json.dumps(fields_dict, indent=2))
-    return fields_dict['Feedback']['Value']
+    return fields_dict
 
 def get_feedback_sentiment():
     # Authenticate to Text Analytics client
     text_analytics_client = TextAnalyticsClient(endpoint=COGNITIVE_SERVICES_ENDPOINT, credential=AzureKeyCredential(COGNITIVE_SERVICES_KEY))
     # Input for sentiment analysis must be in an array
-    feedback = [extract_document_feedback()]
+    fields_dict = extract_document_feedback()
     # The list contains exactly only one element, hence using "," extracts it rather than having to use result[0] in the code every time
-    result, = text_analytics_client.analyze_sentiment(feedback)
+    result, = text_analytics_client.analyze_sentiment([fields_dict['Feedback']['Value']])
     if not result.is_error:
+        # For logging purpose
         print(f"Document Sentiment: {result.sentiment}")
         print(f"Document Confidence Scores: Positive={result.confidence_scores.positive}; Neutral={result.confidence_scores.neutral}; Negative={result.confidence_scores.negative}")
-        return result.sentiment
+        # Add another field for sentiment results
+        fields_dict['Sentiment'] = {'Result': result.sentiment, 'PositiveScore': result.confidence_scores.positive, 'NeutralScore': result.confidence_scores.neutral, 'NegativeScore': result.confidence_scores.negative}
+        return fields_dict
 
-get_feedback_sentiment()
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/analyze', methods=['POST'])
+def post_example():
+    file = request.files['file']
+    # Do something with the file, such as saving it to disk
+    file.save('surveys/sample.pdf')
+    return get_feedback_sentiment()
+
+if __name__ == '__main__':
+    app.run(debug=True)
